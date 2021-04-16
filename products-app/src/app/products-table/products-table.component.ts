b9b9b53bc6c5ecbbc220b3073c4ade321cc10690
Antonio Rafael <antoniorafaelcl@gmail.com>
2021-04-16 14:27:50 -0300

    Project Products App

---
+++ products-app/src/app/products-table/products-table.component.ts
@@ -0,0 +1,31 @@
+import { ProductService } from './../product.service';
+import { Product } from './../models/product.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+
+@Component({
+  selector: 'app-products-table',
+  templateUrl: './products-table.component.html',
+  styleUrls: ['./products-table.component.css']
+})
+export class ProductsTableComponent implements OnInit {
+
+  @ViewChild(MatTable) datatable: MatTable<any>;
+
+  products: Product[] = [];
+
+  prodColumns: string [] = ["id", "prodname", "price", "description", "department"];
+
+
+  constructor(
+    private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.products = this.productService.getProduct();
+    this.productService.onNewProduct
+        .subscribe((p) => {
+          this.datatable.renderRows();
+        });
+  }
+
+}
