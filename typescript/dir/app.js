f57d0fd725d9af4afecfe551d83a25a7370620f2
Antonio Rafael <antoniorafaelcl@gmail.com>
2021-04-16 13:04:51 -0300

    delete folders

--- typescript/dir/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var mensagem = "Teste";
-var temporadasFriends = 10;
-var estudandoAngular = true;
-var listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
-var listaDeFrutas2 = ['Uva', 'Banana', 'Abacaxi'];
-var listaDeProvas = [7.5, 5, 6];
-var listaDeProvas2 = [7.5, 5, 6];
