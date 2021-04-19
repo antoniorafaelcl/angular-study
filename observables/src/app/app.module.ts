b94b07b7955e1d866cbc13aa48ac130fe7166567
Antonio Rafael <antoniorafaelcl@gmail.com>
2021-04-19 12:22:16 -0300

    Finish Project Observables

--- observables/src/app/app.module.ts
+++ observables/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BasicComponent } from './basic/basic.component';
 import { ColdObservablesComponent } from './cold-observables/cold-observables.component';
 import { HotObservablesIntroComponent } from './hot-observables-intro/hot-observables-intro.component';
 import { HotObservablesComponent } from './hot-observables/hot-observables.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { SubjectChildComponent } from './subjects/subject-child/subject-child.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HotObservablesComponent } from './hot-observables/hot-observables.compo
     BasicComponent,
     ColdObservablesComponent,
     HotObservablesIntroComponent,
-    HotObservablesComponent
+    HotObservablesComponent,
+    SubjectsComponent,
+    SubjectChildComponent
   ],
   imports: [
     BrowserModule,
